b488ccf04f865ab85362ff32f93310ff707f23f5
Chris-1010 <122332721@umail.ucc.ie>
2025-04-12 18:14:05 +0200

    Initial project setup with Gradle configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+import com.android.build.api.dsl.Packaging
+
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.ca.tunaro"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.ca.tunaro"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+
+    packaging {
+        resources.excludes.add("META-INF/DEPENDENCIES")
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.browser:browser:1.8.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("androidx.navigation:navigation-ui:2.7.6")
+    implementation("com.google.code.gson:gson:2.6.1")
+    implementation(files("../spotify-app-remote-release-0.8.0.aar"))
+    implementation("com.squareup.okhttp3:okhttp:4.9.1");
+    implementation("se.michaelthelin.spotify:spotify-web-api-java:8.4.1")
+    implementation("com.spotify.android:auth:1.2.5")
+    implementation("androidx.activity:activity:1.9.1") // Maven dependency
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+}
