f30e4f99b4cc61dea3d1e42ce11a662a9bb056dd
Chris-1010 <122332721@umail.ucc.ie>
2025-05-04 17:05:03 +0200

    Update classes to use `BaseActivity` and `PlaybackManager`

--- app/src/main/java/com/ca/tunaro/LibraryFragment.java
+++ app/src/main/java/com/ca/tunaro/LibraryFragment.java
@@ -197,7 +197,6 @@ public class LibraryFragment extends Fragment implements Library_RecyclerViewInt
 
                         // Navigate to SongView
                         Intent intent = new Intent(requireContext(), SongView.class);
-                        intent.putExtra("source", "library");
                         startActivity(intent);
                     });
                 })
