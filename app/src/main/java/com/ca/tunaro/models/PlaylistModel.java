115d8e4438a468ba60adf1346c73d3813097cb4b
Chris-1010 <chrisahern1000@gmail.com>
2025-07-15 22:39:56 +0200

    Add playlist favouriting logic
    - Users can now mark playlists as "favourite".
    - Favourited playlists are displayed at the top of the playlist view.
    - An icon is displayed on favourited playlists.

--- app/src/main/java/com/ca/tunaro/models/PlaylistModel.java
+++ app/src/main/java/com/ca/tunaro/models/PlaylistModel.java
@@ -10,6 +10,7 @@ public class PlaylistModel {
     int songCount;
     Image[] image;
     ArrayList<SongModel> songs;
+    boolean isFavourite;
 
     public PlaylistModel(String id, String playlistName, int songCount, Image[] image, ArrayList<SongModel> songs) {
         this.id = id;
@@ -17,7 +18,10 @@ public class PlaylistModel {
         this.songCount = songCount;
         this.image = image;
         this.songs = songs;
+        this.isFavourite = false;
     }
+
+    //#region Getters
 
     public String getPlaylistName() {
         return playlistName;
@@ -37,8 +41,21 @@ public class PlaylistModel {
 
     public ArrayList<SongModel> getSongs() { return songs; }
 
+    public boolean isFavourite() {
+        return isFavourite;
+    }
+
+    //#endregion
+
+    //#region Setters
+
     public void setSongs(ArrayList<SongModel> songs) {
         this.songs = songs;
     }
 
+    public void setFavourite(boolean favourite) {
+        isFavourite = favourite;
+    }
+
+    //#endregion
 }
