3db9f53042243a8e00f148abf79133b1ec898662
Christopher Ahern <115749466+Chris-1010@users.noreply.github.com>
2025-07-16 19:04:40 +0200

    Add quick play via long-press on album art (#51)
    This commit introduces a quick play feature that allows users to start
    playing a song by long-pressing its album cover in the `PlaylistView`.
    
    - Incremented app version to 2.16.

--- app/src/main/java/com/ca/tunaro/interfaces/Song_RecyclerViewInterface.java
+++ app/src/main/java/com/ca/tunaro/interfaces/Song_RecyclerViewInterface.java
@@ -4,4 +4,7 @@ import android.view.View;
 
 public interface Song_RecyclerViewInterface {
     void onItemClick(int position, View itemView);
+
+    // Quick play functionality
+    default void onAlbumCoverLongClick(int position) {}
 }
